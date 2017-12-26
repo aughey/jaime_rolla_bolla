de7efa70aafd869fc9efc07a4aa7fe62346a7602
John Aughey <jha@aughey.com>
2017-12-26 14:50:16 -0600

    moving

--- DemoReelJaime/DemoReelJaime.ino
+++ DemoReelJaime/DemoReelJaime.ino
@@ -3,21 +3,11 @@
 
 FASTLED_USING_NAMESPACE
 
-// FastLED "100-lines-of-code" demo reel, showing just a few 
-// of the kinds of animation patterns you can quickly and easily 
-// compose using FastLED.  
-//
-// This example also shows one easy way to define multiple 
-// animations patterns and have them automatically rotate.
-//
-// -Mark Kriegsman, December 2014
-
 #if defined(FASTLED_VERSION) && (FASTLED_VERSION < 3001000)
 #warning "Requires FastLED 3.1 or later; check github for latest code."
 #endif
 
 #define DATA_PIN    4
-//#define CLK_PIN   4
 #define LED_TYPE    WS2812B
 #define COLOR_ORDER GRB
 // 98 on long side
